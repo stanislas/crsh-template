1a31e960ba93874d804a2937d795b666662f4b0b
Stanislas Nanchen <stan@hood.ch>
2015-05-16 10:31:04 +0200

    Initial Commit
    Signed-off-by: Stanislas Nanchen <stan@hood.ch>

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject crsh/lein-template "0.1.0"
+  :description "A template for the programming of shell scripts in clojure"
+  :url "https://github.com/stanislas/crsh-template"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
